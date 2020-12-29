ea9f0ebc30cb72e977019a5bb39fa635a8d2243f
luckes-yang <xie_zhongyang@qq.com>
2020-12-29 16:01:53 +0800

    一些修改

--- package/scripts/params.py
+++ package/scripts/params.py
@@ -12,12 +12,10 @@ tmp_dir = Script.get_tmp_dir()
 stack_name = default("/hostLevelParams/stack_name", None)
 
 master_env = config['configurations']['kudu-master-env']
-for key in master_env:
-    exec("{} = {}".format(key, master_env[key]))
+locals().update(master_env)
 
 tserver_env = config['configurations']['kudu-tserver-env']
-for key in tserver_env:
-    exec("{} = {}".format(key, tserver_env[key]))
+locals().update(tserver_env)
 
 current_host_name = socket.gethostname()
 hdfs_host = default("/clusterHostInfo/namenode_hosts", [''])[0]
