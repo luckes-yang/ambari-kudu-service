26df4e75c2e457a87d481a5688e79f3ed9cc38c8
luckes-yang <xie_zhongyang@qq.com>
2020-12-30 18:18:56 +0800

    修改变量生成

--- package/scripts/params.py
+++ package/scripts/params.py
@@ -12,10 +12,7 @@ tmp_dir = Script.get_tmp_dir()
 stack_name = default("/hostLevelParams/stack_name", None)
 
 master_env = config['configurations']['kudu-master-env']
-locals().update(master_env)
-
 tserver_env = config['configurations']['kudu-tserver-env']
-locals().update(tserver_env)
 
 current_host_name = socket.gethostname()
 hdfs_host = default("/clusterHostInfo/namenode_hosts", [''])[0]
