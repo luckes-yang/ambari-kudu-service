07c40d25bcef0eac4fd71ed6d06bb2574a3f6f54
luckes-yang <xie_zhongyang@qq.com>
2020-12-30 18:10:59 +0800

    自动创建数据文件夹

--- package/scripts/kudu-master.py
+++ package/scripts/kudu-master.py
@@ -26,6 +26,16 @@ class KuduMaster(KuduBase):
     def status(self, env):
         check_process_status("/var/run/kudu/kudu-master-kudu.pid")
 
+    def create_dirs(self, data_dir):
+        Directory(data_dir,
+                  create_parents=True,
+                  cd_access="a",
+                  mode=0o755,
+                  owner='kudu',
+                  group='kudu',
+                  ignore_failures=True
+                  )
+
     def configureKuduMaster(self, env):
         import params
         env.set_params(params)
@@ -35,6 +45,8 @@ class KuduMaster(KuduBase):
              content=Template("kudu_master.j2", realm_name=realm_name),
              mode=0o644
              )
+
+        self.create_dirs(params.master_env['fs_wal_dir'])
 
 
 if __name__ == "__main__":
